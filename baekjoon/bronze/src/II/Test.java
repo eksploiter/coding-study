f6fca5fd5a647a0ab969c7acac5bd4ec14f436a9
MinSoo <minsu_510@naver.com>
2024-09-28 01:38:30 +0900

    bronze2_Test

--- baekjoon/bronze/src/II/Test.java
+++ baekjoon/bronze/src/II/Test.java
@@ -77,26 +77,56 @@ import java.util.Scanner;
 
 import java.util.Scanner;
 
+//public class Test {
+//    public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+//
+//        int N = scanner.nextInt();
+//        int count = 0;
+//
+//        for (int i = 0; i < N; i++) {
+//            int num = scanner.nextInt();
+//            if (isPrime(num)) {
+//                count++;
+//            }
+//        }
+//
+//        System.out.println(count);
+//
+//        scanner.close();
+//    }
+//
+//    // 소수를 판별하는 메서드
+//    public static boolean isPrime(int num) {
+//        if (num < 2) { // 2보다 작은 수는 소수가 아님
+//            return false;
+//        }
+//        for (int i = 2; i <= Math.sqrt(num); i++) { // 제곱근까지 확인
+//            if (num % i == 0) {
+//                return false;
+//            }
+//        }
+//        return true;
+//    }
+//}
+
 public class Test {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int M = scanner.nextInt();
         int N = scanner.nextInt();
-        int count = 0;
+        int sum = 0;
 
-        for (int i = 0; i < N; i++) {
-            int num = scanner.nextInt();
-            if (isPrime(num)) {
-                count++;
+        for (int i = M; i <= N; i++) {
+            if (isPrime(i)) {
+                sum += i;
             }
         }
 
-        System.out.println(count);
-
+        System.out.println(sum);
         scanner.close();
     }
-
-    // 소수를 판별하는 메서드
     public static boolean isPrime(int num) {
         if (num < 2) { // 2보다 작은 수는 소수가 아님
             return false;
@@ -109,4 +139,3 @@ public class Test {
         return true;
     }
 }
-
