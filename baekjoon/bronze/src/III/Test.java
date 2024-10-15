fa0a2831121c5ea182c209f7517336e134bc894e
MinSoo <minsu_510@naver.com>
2024-10-16 02:47:41 +0900

    bronze3_test

--- baekjoon/bronze/src/III/Test.java
+++ baekjoon/bronze/src/III/Test.java
@@ -66,27 +66,48 @@ package III;
 
 //scanner.hasNextLine()은 입력이 더 있는지 확인하는 메서드로, 입력이 남아 있는 동안 while 루프가 반복됩니다.
 
+//import java.util.Scanner;
+//
+//public class Test {
+//    public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+//
+//        int T = scanner.nextInt(); // 테스트 케이스 개수 입력받음
+//
+//        for (int i = 0; i < T; i++) {
+//            int C = scanner.nextInt(); // 각 테스트 케이스의 거스름돈 C
+//            int quarters = C / 25; // 쿼터 계산 (25센트)
+//            C %= 25; // 나머지 거스름돈
+//            int dimes = C / 10; // 다임 계산 (10센트)
+//            C %= 10; // 나머지 거스름돈
+//            int nickels = C / 5; // 니켈 계산 (5센트)
+//            C %= 5; // 나머지 거스름돈
+//            int pennies = C; // 페니 계산 (1센트)
+//
+//            System.out.println(quarters + " " + dimes + " " + nickels + " " + pennies);
+//        }
+//
+//        scanner.close();
+//    }
+//}
+
 import java.util.Scanner;
 
 public class Test {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int T = scanner.nextInt(); // 테스트 케이스 개수 입력받음
+        int n = scanner.nextInt();
 
-        for (int i = 0; i < T; i++) {
-            int C = scanner.nextInt(); // 각 테스트 케이스의 거스름돈 C
-            int quarters = C / 25; // 쿼터 계산 (25센트)
-            C %= 25; // 나머지 거스름돈
-            int dimes = C / 10; // 다임 계산 (10센트)
-            C %= 10; // 나머지 거스름돈
-            int nickels = C / 5; // 니켈 계산 (5센트)
-            C %= 5; // 나머지 거스름돈
-            int pennies = C; // 페니 계산 (1센트)
+        System.out.println(executeCount(n));
+        System.out.println(2);
+    }
 
-            System.out.println(quarters + " " + dimes + " " + nickels + " " + pennies);
+    public static long executeCount(int n) {
+        long sum = 0;
+        for (int i = 0; i <= n - 1; i++) {
+            sum += i;
         }
-
-        scanner.close();
+        return sum;
     }
 }
