17ea9de3165d33a53cd70ce65249551e26679a64
Kim Minsoo <minsu_510@naver.com>
2025-02-03 15:34:27 +0900

    Title: 정수 명령 처리 5, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 250203/정수 명령 처리 5/process-numeric-commands-5.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+
+        List<Integer> list = new ArrayLiost<>();
+
+        for (int i = 0; i < N; i++) {
+            String line = scanner.nextLine();
+            String[] parts = line.split(" ");
+
+            switch (parts[0]) {
+                case "pulsh_back":
+                    int target = Integer.parseInt(parts[1]);
+                    list.add(target);
+                    break;
+                case "pop_back":
+                    list.remove(list.size() - 1);
+                    break;
+                case "get":
+                    int index = Integer.parseInt(parts[1]);
+                    System.out.println(list.get(index - 1));
+                    break;
+                case "size":
+                    System.out.prinln(list.size());
+                    break;
+            }
+        }
+    }
+}
